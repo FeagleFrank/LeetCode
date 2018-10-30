3b6a7457ec2872fbb108796507346a9dc0877fd0
feagle-work <feaglefrank@outlook.com>
2018-10-30 11:53:10 +0800

    LeetCode 18

--- 17. Letter Combinations of a Phone Number/solution_1.py
+++ 17. Letter Combinations of a Phone Number/solution_1.py
@@ -19,8 +19,6 @@ class Solution:
         return l
 
 
-
-
 if __name__ == '__main__':
     s = Solution()
     print(s.letterCombinations("23"))
---
+++ 18. 4Sum/solution_1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        def findNsum(l, r, target, N, result, results):
+            if r-l+1 < N or N < 2 or target < nums[l]*N or target > nums[r]*N:
+                return
+            if N == 2:
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(result + [nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else:
+                for i in range(l, r+1):
+                    if i == l or (i > l and nums[i-1] != nums[i]):
+                        findNsum(i+1, r, target-nums[i], N-1, result+[nums[i]], results)
+
+        nums.sort()
+        results = []
+        findNsum(0, len(nums)-1, target, 4, [], results)
+        return results
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.fourSum([1,0,-1,0,-2,2],0))
+
+# 降维思想，将4sum->3sum->2sum
+# Runtime: 88ms
